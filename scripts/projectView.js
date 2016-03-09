35eec0ff647c2c1b75f9f7fa605b420360163338
Zach Simonson <zsimonson26@gmail.com>
2016-03-08 18:33:33 -0800

    handleMainNav created, basic functionality works

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,1 +1,14 @@
 projectView = {};
+
+projectView.handleMainNav = function() {
+  $('.main-nav').on('click', 'li', function(){
+    var $content = $(this).attr('data-content');
+    $('.tab-content').hide();
+    $('.tab-content[id = "'+$content +'"]').fadeIn();
+  });
+  $('.main-nav .tab:first').click();
+};
+
+$(document).ready(function(){
+  projectView.handleMainNav();
+});
