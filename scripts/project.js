8d2ff2d882c52a969982e598083d0cb1d184be99
Zach Simonson <zsimonson26@gmail.com>
2016-03-16 15:31:42 -0700

    wrapped in IIFEs, refactored Project.loadAll to use map

--- scripts/project.js
+++ scripts/project.js
@@ -1,44 +1,53 @@
-function Project(opts){
-  this.title = opts.title;
-  this.projectUrl = opts.projectUrl;
-  this.publishedOn = opts.publishedOn;
-  this.projectDescription = opts.projectDescription;
-}
+(function(module){
+  function Project(opts){
+    this.title = opts.title;
+    this.projectUrl = opts.projectUrl;
+    this.publishedOn = opts.publishedOn;
+    this.projectDescription = opts.projectDescription;
+  }
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function() {
-  var source = $('#project-template').html();
-  var template = Handlebars.compile(source);
-  return template(this);
-}
+  Project.prototype.toHtml = function() {
+    var source = $('#project-template').html();
+    var template = Handlebars.compile(source);
+    return template(this);
+  }
 
-Project.loadAll = function(rawData) {
-  rawData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  })
-};
+//  Project.loadAll = function(rawData) {
+//    rawData.forEach(function(ele) {
+//      Project.all.push(new Project(ele));
+//    })
+//  };
 
-Project.fetchAll = function() {
-  var storedEtag;
-  $.ajax({
-    type: "HEAD",
-    url: "data/portfolioProjects.json",
-    success: function(data, message, xhr) {
-      storedEtag = xhr.getResponseHeader("etag");
-      if (storedEtag === localStorage.etag && localStorage.rawData) {
-        Project.loadAll(JSON.parse(localStorage.rawData));
-        projectView.initIndexPage();
-      } else {
-        $.getJSON('data/portfolioProjects.json', function(data) {
-          console.log(data);
-          Project.loadAll(data);
-          localStorage.setItem("etag", JSON.stringify(storedEtag));
-          localStorage.setItem('rawData', JSON.stringify(data));
+  Project.loadAll = function(rawData){
+    Project.all = rawData.map(function(ele) {
+      return new Project(ele);
+    });
+  };
+
+  Project.fetchAll = function() {
+    var storedEtag;
+    $.ajax({
+      type: "HEAD",
+      url: "data/portfolioProjects.json",
+      success: function(data, message, xhr) {
+        storedEtag = xhr.getResponseHeader("etag");
+        if (storedEtag === localStorage.etag && localStorage.rawData) {
+          Project.loadAll(JSON.parse(localStorage.rawData));
           projectView.initIndexPage();
-          });
+        } else {
+          $.getJSON('data/portfolioProjects.json', function(data) {
+            console.log(data);
+            Project.loadAll(data);
+            localStorage.setItem("etag", JSON.stringify(storedEtag));
+            localStorage.setItem('rawData', JSON.stringify(data));
+            projectView.initIndexPage();
+            });
+        }
       }
-    }
-  });
+    });
 
-};
+  };
+  module.Project = Project;
+}) (window);
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,23 +1,26 @@
-projectView = {};
+(function(module){
+  projectView = {};
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', 'li', function(){
-    var $content = $(this).attr('data-content');
-    $('.tab-content').hide();
-    $('.tab-content[id = "'+$content +'"]').fadeIn();
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', 'li', function(){
+      var $content = $(this).attr('data-content');
+      $('.tab-content').hide();
+      $('.tab-content[id = "'+$content +'"]').fadeIn();
+    });
+          $('.main-nav .tab:first').click();
+  };
+
+  projectView.initIndexPage = function(){
+    Project.all.forEach(function(a){
+      $('#projects').append(a.toHtml());
+    });
+    projectView.handleMainNav();
+  };
+
+  $(document).ready(function(){
+    projectView.handleMainNav();
+      $('article.template').hide();
+      Project.fetchAll();
   });
-        $('.main-nav .tab:first').click();
-};
-
-projectView.initIndexPage = function(){
-  Project.all.forEach(function(a){
-    $('#projects').append(a.toHtml());
-  });
-  projectView.handleMainNav();
-};
-
-$(document).ready(function(){
-  projectView.handleMainNav();
-    $('article.template').hide();
-    Project.fetchAll();
-});
+  module.projectView = projectView;
+})(window);
